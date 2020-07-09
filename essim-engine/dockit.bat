ab7f0b90a6e6f73bab32314600a0220f7eed4e57
Arun Subramanian <arun.subramanian@tno.nl>
2020-07-09 10:33:42 +0200

    Removed unnecessary files from project. Updated POM files for removal of HESI repositories.

--- essim-engine/dockit.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-ECHO ============================
-ECHO Building Docker image
-ECHO ============================
-docker build -t 10.30.2.1:5000/essim-engine:latest .
-ECHO ============================
-ECHO Pushing Docker image
-ECHO ============================
-docker push 10.30.2.1:5000/essim-engine:latest
